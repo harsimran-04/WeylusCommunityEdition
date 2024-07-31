ed6c0d1fd9a0894d61883c5a6f604e2b2bc46019
Richard Smith <github@electronstudio.co.uk>
2024-07-31 13:31:38 +0100

    resolve some merge conflicts

--- patch_community_edition.sh
+++ patch_community_edition.sh
@@ -1,23 +1,36 @@
 git clone https://github.com/H-M-H/Weylus.git
 cd Weylus
+echo "CONTRIBUTORS merge=ours" >.gitattributes
+git config --global merge.ours.driver true
 
 git remote add qdlmcfresh https://github.com/qdlmcfresh/Weylus.git
 git fetch qdlmcfresh stylus_windows
 git merge qdlmcfresh/stylus_windows -m "merged qdlmcfresh"
 
+git remote add qdlmcfreshcompat https://github.com/electronstudio2/WeylusQdlmcFreshLyonbotCompat.git
+git fetch qdlmcfreshcompat stylus_windows
+git merge qdlmcfreshcompat/stylus_windows -m "merged qdlmcfresh compatability"
+
 git remote add OmegaRogue https://github.com/OmegaRogue/Weylus.git
 git fetch OmegaRogue master
-git merge -s recursive -X theirs OmegaRogue/master -m "merged OmegaRogue"
+git merge OmegaRogue/master -m "merged OmegaRogue"
 
 git remote add scribblemaniac https://github.com/scribblemaniac/Weylus.git
 git fetch scribblemaniac fix-qr-code
 git merge scribblemaniac/fix-qr-code -m "merged scribblemaniac"
 
-git remote add Diordany https://github.com/Diordany/Weylus.git
-git fetch Diordany pr-func-proto
-git merge Diordany/pr-func-proto -m "merged Diordany"
+# This one no longer seems necessary?  Breaks mac build with vendored ffmpeg.
+#git remote add Diordany https://github.com/Diordany/Weylus.git
+#git fetch Diordany pr-func-proto
+#git merge Diordany/pr-func-proto -m "merged Diordany"
 
-git remote add lyonbot https://github.com/lyonbot/Weylus.git
+
+# This needs a fix for windows currently
+#git remote add lyonbot https://github.com/lyonbot/Weylus.git
+#git fetch lyonbot pr
+#git merge lyonbot/pr -m "merged lyonbot"
+# The fixed version
+git remote add lyonbot https://github.com/electronstudio2/WeylusLyonbotWindowsFixes.git
 git fetch lyonbot pr
 git merge lyonbot/pr -m "merged lyonbot"
 
@@ -25,5 +38,19 @@ git remote add electronstudio2 https://github.com/electronstudio2/Weylus.git
 git fetch electronstudio2 build-fixes
 git merge electronstudio2/build-fixes -m "merged build-fixes"
 
+
+sed -i '/<<<<<<< HEAD/,/=======/d' .github/workflows/build.yml
+sed -i '/>>>>>>> electronstudio2\/build-fixes/d' .github/workflows/build.yml
+
+sed -i '/<<<<<<< HEAD/d' build.rs
+sed -i '/=======/,/>>>>>>> electronstudio2\/build-fixes/d' build.rs
+sed -i '/>>>>>>> electronstudio2\/build-fixes/d' build.rs
+
+git add build.rs .github/workflows/build.yml
+git commit -m 'resolve conflict'
+
 git fetch electronstudio2 community-edition-patches
 git merge electronstudio2/community-edition-patches -m "merged community-edition-patches"
+
+git remote add wce https://github.com/electronstudio/WeylusCommunityEdition.git
+###git push -u wce master --force
